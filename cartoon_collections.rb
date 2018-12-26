fadccb86d8d0777713841d96345dae70754d0efe
Michael Romero <liikemike@aim.com>
2018-12-26 06:11:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
